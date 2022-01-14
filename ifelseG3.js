7b94b0edf2e6f44327133e0f6cb23846edaaf9b4
virendra uikey <aarushuikey@gmail.com>
2022-01-14 16:38:13 +0530

    there are three variable and which is greater

---
+++ ifelseG3.js
@@ -0,0 +1,12 @@
+var a =40;
+var b = 20;
+var c = 30;
+
+if(a > b && a > c)
+   console.log("A is greater ");
+    else if(b > a && b > c)
+       console.log("B is greater ");
+           else if(c > a && c > b)
+               console.log("c is greater");
+               else
+                 console.log("please enter the diffrent value");
