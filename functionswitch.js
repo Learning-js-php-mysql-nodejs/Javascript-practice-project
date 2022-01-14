5333db13b8cdb1cd6d952eccc019e5462b5037ab
virendra uikey <aarushuikey@gmail.com>
2022-01-14 18:37:10 +0530

    switch function

---
+++ functionswitch.js
@@ -0,0 +1,54 @@
+
+var number = 2;
+ switch (number)
+ {
+     case 1: 
+     var k = 1;
+     first(k);
+     
+     break;
+     
+     case 2: 
+     var k = 2;
+     first(k);
+
+      
+     break;
+     
+
+     case 3: 
+     var k = 3;
+     first(k);
+         
+     break;
+     
+     default: 
+         console.log("please enter valid number");
+     
+ }
+
+
+function first(veriable){
+    switch(veriable){
+
+        case 1: 
+          console.log("variable first");
+
+        break;
+
+        case 2: 
+          console.log("variable second");
+
+        break;
+
+        case 3: 
+          console.log("variable third");
+
+        break;
+
+        default:
+            console.log("variable not here");
+
+
+    }
+}
---
+++ nestedif.js
@@ -0,0 +1,13 @@
+var age = 50;
+
+if ( age >= 14 && age <= 20 )
+   console.log("you are teenager");
+   else
+   {
+       if( age >= 21 && age <= 25)
+         console.log("you are adult");
+         else if( age >= 26 && age <= 40)
+         console.log("you are youngester");
+            else if ( age >= 41 && age <= 100);
+            console.log(" you senior citizan");
+   }
